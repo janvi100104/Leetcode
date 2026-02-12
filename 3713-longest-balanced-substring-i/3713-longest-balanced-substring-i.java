6e6cd15375221d0aa52e3200bc21051b08f83842
Janvi Singhal <janvisinghal10@gmail.com>
2026-02-12 20:12:39 +0530

    Time: 127 ms (73.22%), Space: 46.9 MB (62.55%) - LeetHub

---
+++ 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.java
@@ -0,0 +1,35 @@
+class Solution {
+
+    public boolean checkBalance(int[] freq) {
+        int common = 0;
+
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] == 0) continue;
+
+            if (common == 0) {
+                common = freq[i];
+            } else if (freq[i] != common) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public int longestBalanced(String s) {
+        int maxL = 0;
+        int n = s.length();
+
+        for (int i = 0; i < n; i++) {
+            int[] freq = new int[26];
+
+            for (int j = i; j < n; j++) {
+                freq[s.charAt(j) - 'a']++;
+
+                if (checkBalance(freq)) {
+                    maxL = Math.max(maxL, j - i + 1);
+                }
+            }
+        }
+        return maxL;
+    }
+}
